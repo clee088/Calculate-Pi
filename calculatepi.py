72d87b0f3434575d2fe0ef1412fa5e99ae26ee86
clee088 <42843045+clee088@users.noreply.github.com>
2018-09-04 17:25:14 -0400

    Updated from Brython Server: 9/4/2018 5:25:14 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
 Author: Christopher Lee
-Credit: <list sources used, if any>
+Credit: Noah
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
